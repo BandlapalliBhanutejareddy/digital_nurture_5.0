eac05cbb899a3700c43f10d4bb8c62e42b1ab8c2
Cognizant Digital Nurture <nurture@cognizant.com>
2026-07-23 18:30:07 +0530

    Add missing Maven scaffolding for later module exercises

---
+++ Week-4/Module-8-Spring-REST-using-Spring-Boot/Exercises/Exercise-1-REST-Controller/src/main/java/Main.java
@@ -0,0 +1,7 @@
+﻿package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercise-1-REST-Controller exercise ready.");
+    }
+}
---
+++ Week-6/Module-14-Cloud-Fundamentals/Exercises/Exercise-1-Cloud-Concepts/src/main/java/Main.java
@@ -0,0 +1,7 @@
+﻿package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercise-1-Cloud-Concepts exercise ready.");
+    }
+}
